39a159f1743448ef62981aa0c0adf4b04a9ac305
Jason Ding <Ventues7@gmail.com>
2020-07-15 10:16:09 -0700

    Tried out Leo's song editor, need to check convertAudioBufferToBlob

--- client/src/components/Widgets/TextToSpeechIBMWidget.tsx
+++ client/src/components/Widgets/TextToSpeechIBMWidget.tsx
@@ -1,7 +1,73 @@
 import "../css/Widget.css";
-
 import React, { useEffect, useState } from "react";
+import { TRACK_LENGTH_MODIFIDER, convertAudioBufferToBlob } from "./util";
+
+var Sound = require("react-sound").default;
+var createBuffer = require("audio-buffer-from");
+
+const TextToSpeechV1 = require("ibm-watson/text-to-speech/v1");
+const { IamAuthenticator } = require("ibm-watson/auth");
+
+const apikey = "v9V7IbRttLFzRqM4xkSMUja7VxHSR6VstOu1tA_b3A91";
+const apiurl =
+  "https://api.us-south.text-to-speech.watson.cloud.ibm.com/instances/02167163-0352-4a3b-b438-df3108c67fb2";
+
+const textToSpeech = new TextToSpeechV1({
+  authenticator: new IamAuthenticator({
+    apikey: apikey,
+  }),
+  url: apiurl,
+});
+const synthesizeParams = {
+  text: "Hello world",
+  accept: "audio/mp3",
+  voice: "en-US_AllisonV3Voice",
+};
+
+var context: AudioContext;
+window.addEventListener("load", init, false);
+function init() {
+  console.log("Initialize");
+  try {
+    // Fix up for prefixing
+    window.AudioContext = window.AudioContext;
+    context = new AudioContext();
+  } catch (e) {
+    alert("Web Audio API is not supported in this browser");
+  }
+}
 
 export const TextToSpeechIBMWidget = () => {
-  return <div>Hello</div>;
+  const [audio, setAudio] = useState(new Audio());
+  const [isPlayingSong, setPlayingSong] = useState(false);
+
+  const getTextToSpeech = async () => {
+    textToSpeech
+      .synthesize(synthesizeParams)
+      .then((response: { result: any }) => {
+        // obtain the audio stream
+        return response.result;
+      })
+      .then((buffer: any) => {
+        console.log(buffer);
+        const blob = convertAudioBufferToBlob(buffer);
+        const newAudioUrl = URL.createObjectURL(blob);
+        const newAudio = new Audio(newAudioUrl);
+        setAudio(newAudio);
+        newAudio.play();
+        setPlayingSong(true);
+      })
+      .catch((err: any) => {
+        console.log("error:", err);
+      });
+  };
+
+  return (
+    <div>
+      <button id="loginButton" onClick={getTextToSpeech}>
+        TEST
+      </button>
+      <audio controls id="audio"></audio>
+    </div>
+  );
 };
---
+++ client/src/components/Widgets/types/index.ts
@@ -0,0 +1,71 @@
+import WaveformData from "waveform-data";
+
+export interface userSong {
+  authorName: string;
+  songName: string;
+  fullPath: string;
+  id: string;
+  url: string;
+  gifUrl?: string;
+  gifId?: string;
+}
+
+export interface ILibraryMetadata {
+  name: string;
+  downloadURL: string;
+}
+
+export enum WidgetTypes {
+  time = "time",
+  joke = "joke",
+  shareSong = "share song",
+}
+
+export interface UserFile {
+  file: File | { name: string };
+  waveformData: WaveformData;
+  id: string;
+  audioBuffer: AudioBuffer;
+}
+
+export interface UserFiles {
+  [key: string]: UserFile;
+}
+
+export const ItemTypes = {
+  BOX: "BOX",
+  TRACK: "TRACK",
+  WIDGET: "WIDGET",
+};
+
+export interface DragItem {
+  type: string;
+  id: string;
+  top: number;
+  left: number;
+}
+
+export interface DragItemTrack {
+  index: number;
+  id: string;
+  type: string;
+}
+
+export interface ITrack {
+  id: string;
+  waveformData: WaveformData;
+  referenceId: string;
+}
+
+export enum ACTIONS {
+  dragAndDropFile = "drag and drop file",
+  selectFromFolder = "select from folder",
+  //   selectFromLibrary = "select from library",
+}
+
+export interface IUserUpload {
+  _id: string;
+  songName: string;
+  authorName: string;
+  url: string;
+}
