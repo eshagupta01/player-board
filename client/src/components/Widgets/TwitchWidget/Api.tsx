f77b51df6f6693f38093d88c20aeeaae83a33277
Jason Ding <Ventues7@gmail.com>
2020-07-15 14:03:23 -0700

    Able to retrieve games from twitch

--- client/src/components/Widgets/TwitchWidget/Api.tsx
+++ client/src/components/Widgets/TwitchWidget/Api.tsx
@@ -3,6 +3,7 @@ import axios from "axios";
 let api = axios.create({
   headers: {
     "Client-ID": "86btiuz5lua5j1h9myg67f5qh8pamo",
+    Accept: "application/vnd.twitchtv.v5+json",
   },
 });
 
--- client/src/components/Widgets/TwitchWidget/Games.tsx
+++ client/src/components/Widgets/TwitchWidget/Games.tsx
@@ -1,6 +1,19 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import api from "./Api";
 function Games() {
-  return <div>game component</div>;
+  const url = "https://api.twitch.tv/kraken/games/top";
+  const [games, setGames] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await api.get(url);
+      console.log(result);
+    };
+    fetchData();
+  });
+  return (
+    <div>
+      <h1>Most Popular Games</h1>
+    </div>
+  );
 }
 export default Games;
